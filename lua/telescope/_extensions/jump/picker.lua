1557474a8eb30178bc6e64711ed73d509e60d506
anekos <anekos@snca.net>
2024-08-09 11:03:52 +0900

    Add `dd` mapping to delete selected path from jump list

--- lua/telescope/_extensions/jump/picker.lua
+++ lua/telescope/_extensions/jump/picker.lua
@@ -5,8 +5,7 @@ local previewers = require('telescope.previewers')
 local sorters = require('telescope.sorters')
 local state = require('telescope.actions.state')
 
-
-return function (opts)
+return function(opts)
   opts = opts or {}
 
   local results = {}
@@ -14,28 +13,41 @@ return function (opts)
     table.insert(results, vim.fn.fnamemodify(dir, ':~'))
   end
 
-  pickers.new(opts, {
-    prompt_title = 'jump',
-    finder = finders.new_table {
-      results = results,
-      entry_maker = function (dir)
-        return {
-          value = dir,
-          display = dir,
-          ordinal = dir,
-          path = dir,
-        }
-      end
-    },
-    sorter = sorters.fuzzy_with_index_bias(),
-    previewer = previewers.cat.new({}),
-    attach_mappings = function(prompt_bufnr, map)
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        local selection = state.get_selected_entry()
-        vim.cmd('cd ' .. selection.value)
-      end)
-      return true
-    end,
-  }):find()
+  local entry_maker = function(dir)
+    return {
+      value = dir,
+      display = dir,
+      ordinal = dir,
+      path = dir,
+    }
+  end
+
+  pickers
+    .new(opts, {
+      prompt_title = 'jump',
+      finder = finders.new_table {
+        results = results,
+        entry_maker = entry_maker,
+      },
+      sorter = sorters.fuzzy_with_index_bias(),
+      previewer = previewers.cat.new {},
+      attach_mappings = function(prompt_bufnr, map)
+        actions.select_default:replace(function()
+          actions.close(prompt_bufnr)
+          local selection = state.get_selected_entry()
+          vim.cmd('cd ' .. selection.value)
+        end)
+
+        map('n', 'dd', function()
+          local current_picker = state.get_current_picker(prompt_bufnr)
+          current_picker:delete_selection(function(_)
+            local path = state.get_selected_entry().value
+            vim.fn.system('jump forget ' .. vim.fn.fnameescape(path))
+            print('Forget item: ' .. path)
+          end)
+        end)
+        return true
+      end,
+    })
+    :find()
 end
